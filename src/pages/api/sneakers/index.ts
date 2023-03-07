ca16ea9a84d00bf5d8245c6fbe7f0c78681d9d47
Klaurrr <usachev-85@yandex.ru>
2023-03-07 18:19:30 +0300

    played with api

--- src/pages/api/sneakers/index.ts
+++ src/pages/api/sneakers/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { sneakers } from "database";
+import { sneakers } from "db.json";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(sneakers);
